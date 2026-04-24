a0c0392e4726798feeb30f3199d523e525b6787a
Douglas <dodomathias29@gmail.com>
2026-04-23 22:12:00 -0300

    criei a classe faturadao

--- SistemaHospitalar/src/br/com/SistemaHospitalar/classes/Leito.java
+++ SistemaHospitalar/src/br/com/SistemaHospitalar/classes/Leito.java
@@ -6,15 +6,23 @@ public class Leito {
     private Integer id;
     private StatusLeito statusLeito;
     private Ala ala;
+
+    public Leito(Integer id, StatusLeito statusLeito, Ala ala) {
+        this.id = id;
+        this.statusLeito = statusLeito;
+        this.ala = ala;
+    }
+
     public Integer getId() {
         return id;
     }
+
     public StatusLeito getStatusLeito() {
         return statusLeito;
     }
+
     public Ala getAla() {
         return ala;
     }
 
-    
 }
---
+++ SistemaHospitalar/src/br/com/SistemaHospitalar/persistence/FaturaDao.java
@@ -0,0 +1,5 @@
+package br.com.SistemaHospitalar.persistence;
+
+public class FaturaDao {
+    
+}
